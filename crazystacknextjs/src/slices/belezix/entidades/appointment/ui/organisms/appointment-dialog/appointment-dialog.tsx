ad5fda9c16769e9c1a473645d206819f7050a52d
gustavo.aguiar <imartytk@hotmail.com>
2024-11-01 10:32:11 -0300

    Refatorar componente ScrollWrapper para adicionar setas de navegação

--- crazystacknextjs/src/slices/belezix/entidades/appointment/ui/organisms/appointment-dialog/appointment-dialog.tsx
+++ crazystacknextjs/src/slices/belezix/entidades/appointment/ui/organisms/appointment-dialog/appointment-dialog.tsx
@@ -1,6 +1,13 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import {
   Sheet,
   SheetContent,
   SheetFooter,
@@ -8,7 +15,9 @@ import {
   SheetTitle,
 } from "@/components/ui/sheet";
 import { useAuth } from "@/shared/libs/contexts/AuthContext";
+import { useGetUsers } from "@/slices/general/entidades/user";
 import { useRouter } from "next/navigation";
+import { useMemo, useState } from "react";
 
 interface AppointmentDialogProps {
   service: {
@@ -33,7 +42,21 @@ export const AppointmentDialog: React.FC<AppointmentDialogProps> = ({
 }) => {
   const { user } = useAuth();
   const router = useRouter();
-  const professionals = [];
+  const [selectedDay, setSelectedDay] = useState<Date | undefined>(undefined);
+  const [selectedTime, setSelectedTime] = useState<string | undefined>(
+    undefined,
+  );
+  const [selectedProfessional, setSelectedProfessional] = useState<
+    string | undefined
+  >(undefined);
+
+  const { data: userData } = useGetUsers(
+    1,
+    { enabled: appointmentSheetIsOpen },
+    { role: "professional", ownerId: owner?._id },
+  );
+  const professionals = useMemo(() => userData?.users, [userData]);
+  console.log({ professionals });
   const handleCreateAppointment = () => {};
   const isConfirmButtonDisabled = true;
   return (
@@ -42,10 +65,28 @@ export const AppointmentDialog: React.FC<AppointmentDialogProps> = ({
       onOpenChange={setAppointmentSheetIsOpen}
     >
       <SheetContent className="px-0">
-        <SheetHeader>
+        <SheetHeader className="lg:ml-4">
           <SheetTitle>Fazer Agendamento</SheetTitle>
         </SheetHeader>
-        {professionals?.length > 0 && <div></div>}
+        {professionals?.length > 0 && (
+          <div className="p-5">
+            <Select
+              onValueChange={setSelectedProfessional}
+              value={selectedProfessional}
+            >
+              <SelectTrigger>
+                <SelectValue placeholder="Selecione um profissional" />
+              </SelectTrigger>
+              <SelectContent>
+                {professionals.map((professional: any) => (
+                  <SelectItem key={professional._id} value={professional._id}>
+                    {professional.name}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+          </div>
+        )}
         <SheetFooter className="mt-5 px-5">
           <Button
             onClick={handleCreateAppointment}
