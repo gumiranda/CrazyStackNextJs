f04f24ee8d24d76f425ccbe0b02d90c0457ef74e
gustavo.aguiar <gustavo.aguiar@redspark.io>
2025-03-05 11:39:19 -0300

    Adicionar página de verificação de email e componente de fallback para gerenciamento de verificação de conta

--- crazystacknextjs/src/shared/libs/contexts/AuthContext.tsx
+++ crazystacknextjs/src/shared/libs/contexts/AuthContext.tsx
@@ -278,3 +278,19 @@ const formatMessage = (message: string) => {
       return message;
   }
 };
+export const verifyEmail = async ({
+  email,
+  token,
+}: {
+  email: string;
+  token: string;
+}) => {
+  if (!api) {
+    throw new Error("API is not initialized");
+  }
+  const response = await api.post("auth/verify-email", {
+    email,
+    token,
+  });
+  return response;
+};
