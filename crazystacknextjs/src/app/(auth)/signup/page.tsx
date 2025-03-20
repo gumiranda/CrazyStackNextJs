6a50069811513496810d58530bd2fd62402e9878
gustavo.aguiar <gustavo.aguiar@redspark.io>
2025-03-20 15:11:30 -0300

    Atualizar página de cadastro para incluir componentes SideDiv e SignUpDiv, melhorando a estrutura do layout e a organização do formulário de inscrição.

--- crazystacknextjs/src/app/(auth)/signup/page.tsx
+++ crazystacknextjs/src/app/(auth)/signup/page.tsx
@@ -1,6 +1,8 @@
 import { whitelabel } from "@/application/whitelabel";
 import { SignUpForm } from "@/slices/general/features/signup/signup-form";
 import type { Metadata } from "next";
+import { SignUpDiv } from "./_components/signup-div";
+import { SideDiv } from "../_components/side-div";
 
 export const metadata: Metadata = {
   title: `${whitelabel.systemName} | Cadastro`,
@@ -8,5 +10,10 @@ export const metadata: Metadata = {
 };
 
 export default function SignUpPage() {
-  return <SignUpForm />;
+  return (
+    <div className="container relative h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <SideDiv />
+      <SignUpDiv />
+    </div>
+  );
 }
