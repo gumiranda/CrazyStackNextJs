75c3647113482e9c59138c0312a9195a41fa0b2c
gustavo.aguiar <gustavo.aguiar@redspark.io>
2025-03-20 15:36:56 -0300

    Refatorar a lógica de verificação de email para simplificar o código e melhorar a legibilidade, removendo excessos e organizando a estrutura da função.

--- crazystacknextjs/src/shared/libs/contexts/verify-email.tsx
+++ crazystacknextjs/src/shared/libs/contexts/verify-email.tsx
@@ -5,25 +5,21 @@ export const verifyEmail = async ({
   email: string;
   token: string;
 }) => {
-  try {
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_API_URL}/auth/verify-email`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ email, code: token }),
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/auth/verify-email`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
       },
-    );
-    if (!response.ok) {
-      return;
-    }
-    const data = await response.json();
-    console.log(data);
-    return data;
-  } catch (error) {
-    console.log(error);
-    return null;
+      body: JSON.stringify({ email, code: token }),
+    },
+  );
+  if (!response.ok) {
+    console.log("response");
+    return;
   }
+  const data = await response.json();
+  console.log(data);
+  return data;
 };
