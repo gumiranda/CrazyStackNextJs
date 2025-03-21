baa9f6731dfb94764453d936e995bb85a609c041
gustavo.aguiar <gustavo.aguiar@redspark.io>
2025-03-21 10:05:34 -0300

    Adicionar lógica para carregar tweets na página inicial, incluindo tratamento de erros e exibição de um formulário para novos tweets. Melhorar a estrutura da página com seções distintas para agendamentos e feed de tweets.

--- crazystacknextjs/src/app/(pageswithheader)/(home)/page.tsx
+++ crazystacknextjs/src/app/(pageswithheader)/(home)/page.tsx
@@ -7,6 +7,9 @@ import { getRequests } from "@/slices/belezix/entidades/request/request.api";
 import { startOfDay } from "date-fns";
 import { HorizontalList } from "../_components/templates/horizontal-list";
 import { getCookies } from "@/shared/libs/utils/cookies";
+import { TweetFormContainer } from "./_components/molecules/tweet-form";
+import { TweetList } from "./_components/molecules/tweet-list";
+import { getTweets } from "@/slices/belezix/entidades/tweet/tweet.api";
 
 export const metadata: Metadata = {
   title: `${whitelabel.systemName} | Agendamentos Online`,
@@ -38,6 +41,18 @@ async function handleRequests(cookies: any) {
     return { requests: [], totalCount: 0 };
   }
 }
+async function handleTweets(cookies: any) {
+  try {
+    const { tweets = [], totalCount = 0 } = await getTweets(1, cookies, {
+      sortBy: "createdAt",
+      typeSort: "desc",
+      tweetId: "null",
+    });
+    return { tweets, totalCount };
+  } catch (error: any) {
+    return { tweets: [], totalCount: 0 };
+  }
+}
 export default async function Page() {
   const cookies = await getParsedCookies();
   const popularOwners = await getOwnersPublic(1, cookies, {
@@ -53,31 +68,45 @@ export default async function Page() {
     : [];
   const newOwners = Array.isArray(lastOwners?.owners) ? lastOwners?.owners : [];
   const { requests, totalCount } = await handleRequests(cookies);
+  const { tweets, totalCount: countTweets } = await handleTweets(cookies);
+
   return (
-    <div className="flex flex-col xl:flex-row lg:justify-around">
-      <div className="w-full lg:w-auto">
-        <SubHeader />
-        <HorizontalList
-          title="Agendamentos"
-          array={requests}
-          type="appointment"
-          widthCard={342}
-        />
-      </div>
-      <div className="w-full lg:w-auto">
-        <HorizontalList
-          title="Novos"
-          array={newOwners}
-          type="owner"
-          widthCard={200}
-        />
-        <HorizontalList
-          title="Populares"
-          array={owners}
-          type="owner"
-          widthCard={200}
-        />
-      </div>
-    </div>
+    <main className="min-h-screen flex flex-col xl:flex-row justify-center mx-auto">
+      <section className="flex flex-col w-full px-3 border-r-2 border-gray-900">
+        <div className="xl:mx-16 my-5">
+          <SubHeader />
+          <HorizontalList
+            title="Agendamentos"
+            array={requests}
+            type="appointment"
+            widthCard={342}
+          />
+        </div>
+        <div className="xl:mx-16">
+          <HorizontalList
+            title="Novos"
+            array={newOwners}
+            type="owner"
+            widthCard={200}
+          />
+          <HorizontalList
+            title="Populares"
+            array={owners}
+            type="owner"
+            widthCard={200}
+          />
+        </div>
+      </section>
+      <section className="flex flex-col w-full">
+        <div className="mx-10 xl:mx-16 my-10">
+          <h2 className="text-xl font-bold font-inter">Feed</h2>
+          <p>Veja os posts dos seus amigos e participe da conversa</p>
+          <TweetFormContainer />
+        </div>
+        <div className="mx-10 xl:mx-16 flex flex-col items-center justify-center space-y-4">
+          <TweetList initialTweets={tweets as any} countTweets={countTweets} />
+        </div>
+      </section>
+    </main>
   );
 }
