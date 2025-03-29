8884cff5942f9d4a5ca057bbd976e3a978121468
gustavoaguiar <gustavo.aguiar@redspark.io>
2025-03-29 07:49:27 -0300

    fix

--- crazystacknextjs/src/app/(pageswithheader)/(home)/page.tsx
+++ crazystacknextjs/src/app/(pageswithheader)/(home)/page.tsx
@@ -10,109 +10,138 @@ import { TweetFormContainer } from "./_components/molecules/tweet-form";
 import { TweetList } from "./_components/molecules/tweet-list";
 import { getTweets } from "@/slices/belezix/entidades/tweet/tweet.api";
 import { getCookies } from "@/shared/libs/utils/cookies";
-export const metadata: Metadata = {
-  title: `${whitelabel.systemName} | Agendamentos Online`,
-  description: `Página de inicial do ${whitelabel.systemName}. Aqui você pode agendar com os melhores estabelecimentos da cidade.`,
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
+
+const clearServerCookies = async () => {
+  const cookieStore = await cookies();
+  const allCookies = await cookieStore.getAll();
+
+  for (const cookie of allCookies) {
+    cookieStore.set(cookie.name, '', { maxAge: 0 });
+  }
 };
 
 async function getParsedCookies() {
-  const cookies = await getCookies();
-  if (!cookies) {
-    return null;
+   const cookies = await getCookies();
+   if (!cookies) {
+     return null;
   }
   const parsedCookies = parseCookies(cookies);
-  if (!parsedCookies?.["belezixclient.token"]) {
-    return null;
+   if (!parsedCookies?.["belezixclient.token"]) {
+     return null;
   }
   return parsedCookies;
 }
+
 async function handleRequests(cookies: any) {
-  try {
-    const { requests, totalCount = 0 } = !cookies
+   try {
+    const result = !cookies
       ? { requests: [], totalCount: 0 }
       : await getRequests(1, cookies, {
           initDate: startOfDay(new Date()).toISOString(),
           sortBy: "initDate",
           typeSort: "desc",
         });
-    return { requests, totalCount };
+     return result;
   } catch (error: any) {
+    //await clearServerCookies();
     return { requests: [], totalCount: 0 };
   }
 }
+
 async function handleTweets(cookies: any) {
-  try {
-    const { tweets = [], totalCount = 0 } = await getTweets(1, cookies, {
+   try {
+    const result = await getTweets(1, cookies, {
       sortBy: "createdAt",
       typeSort: "desc",
       tweetId: "null",
-    });
-    return { tweets, totalCount };
+    }) || { tweets: [], totalCount: 0 };
+     return result;
   } catch (error: any) {
+    //await clearServerCookies();
     return { tweets: [], totalCount: 0 };
   }
 }
-export default async function Page() {
-  const cookies = await getParsedCookies();
-  const popularOwners = await getOwnersPublic(1, cookies, {
-    sortBy: "appointmentsTotal",
-    typeSort: "desc",
-  });
-  const lastOwners = await getOwnersPublic(1, cookies, {
-    sortBy: "createdAt",
-    typeSort: "desc",
-  });
 
-  const owners = Array.isArray(popularOwners?.owners)
-    ? popularOwners?.owners
-    : [];
-  const newOwners = Array.isArray(lastOwners?.owners) ? lastOwners?.owners : [];
-  const { requests, totalCount } = await handleRequests(cookies);
-  const { tweets, totalCount: countTweets } = await handleTweets(cookies);
-  return (
-    <>
-      <main className="min-h-screen flex flex-col xl:flex-row justify-center mx-auto">
-        <section className="flex flex-col w-full px-3 border-r-2 border-gray-900">
-          <div className="xl:mx-16 my-5">
-            <SubHeader />
-            <HorizontalList
-              title="Agendamentos"
-              array={requests}
-              type="appointment"
-              widthCard={342}
-            />
-          </div>
-          <div className="xl:mx-16">
-            <HorizontalList
-              title="Novos"
-              array={newOwners}
-              type="owner"
-              widthCard={200}
-            />
-            <HorizontalList
-              title="Populares"
-              array={owners}
-              type="owner"
-              widthCard={200}
-            />
-          </div>
-        </section>
-        <section className="flex flex-col w-full">
-          <div className="mx-10 xl:mx-16 my-10">
-            <h2 className="text-xl font-bold font-inter">Feed</h2>
-            <p>
-              Veja o que as pessoas estão falando sobre o Belezix e participe
-            </p>
-            <TweetFormContainer />
-          </div>
-          <div className="mx-10 xl:mx-16 flex flex-col items-center justify-center space-y-4">
-            <TweetList
-              initialTweets={tweets as any}
-              countTweets={countTweets}
-            />
-          </div>
-        </section>
-      </main>
-    </>
-  );
+export const metadata: Metadata = {
+  title: `${whitelabel.systemName} | Agendamentos Online`,
+  description: `Página de inicial do ${whitelabel.systemName}. Aqui você pode agendar com os melhores estabelecimentos da cidade.`,
+};
+
+export default async function Page() { 
+  try {
+    const cookies = await getParsedCookies(); 
+    if (!cookies) {
+     // await clearServerCookies();
+      return null;
+    }
+
+    const popularOwners = await getOwnersPublic(1, cookies, {
+      sortBy: "appointmentsTotal",
+      typeSort: "desc",
+    }); 
+    const lastOwners = await getOwnersPublic(1, cookies, {
+      sortBy: "createdAt",
+      typeSort: "desc",
+    });
+
+    const owners = Array.isArray(popularOwners?.owners)
+      ? popularOwners?.owners
+      : [];
+    const newOwners = Array.isArray(lastOwners?.owners) ? lastOwners?.owners : [];
+  
+    const { requests, totalCount } = await handleRequests(cookies);
+  
+    const { tweets, totalCount: countTweets } = await handleTweets(cookies);
+ 
+    return (
+      <>
+        <main className="min-h-screen flex flex-col xl:flex-row justify-center mx-auto">
+          <section className="flex flex-col w-full px-3 border-r-2 border-gray-900">
+            <div className="xl:mx-16 my-5">
+              <SubHeader />
+              <HorizontalList
+                title="Agendamentos"
+                array={requests}
+                type="appointment"
+                widthCard={342}
+              />
+            </div>
+            <div className="xl:mx-16">
+              <HorizontalList
+                title="Novos"
+                array={newOwners}
+                type="owner"
+                widthCard={200}
+              />
+              <HorizontalList
+                title="Populares"
+                array={owners}
+                type="owner"
+                widthCard={200}
+              />
+            </div>
+          </section>
+          <section className="flex flex-col w-full">
+            <div className="mx-10 xl:mx-16 my-10">
+              <h2 className="text-xl font-bold font-inter">Feed</h2>
+              <p>
+                Veja o que as pessoas estão falando sobre o Belezix e participe
+              </p>
+              <TweetFormContainer />
+            </div>
+            <div className="mx-10 xl:mx-16 flex flex-col items-center justify-center space-y-4">
+              <TweetList
+                initialTweets={tweets as any}
+                countTweets={countTweets}
+              />
+            </div>
+          </section>
+        </main>
+      </>
+    );
+  } catch (error: any) {
+    //await clearServerCookies();
+  }
 }
